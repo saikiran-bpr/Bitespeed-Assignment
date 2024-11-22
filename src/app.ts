c695b9cdde692af0f4ca3e3caed49bb7f957ccb7
Bompelliwar Sai Kiran <bompelliwarsaikiran@Sai-Kirans-MacBook-Pro.local>
2024-11-23 01:49:43 +0530

    database setup

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import dotenv from 'dotenv';
 import cors from "cors";
-
+import routes from "../src/routes";
 dotenv.config();
 const app : Application = express();
 const PORT = process.env.PORT || 3000;
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-
+app.use("/api", routes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
