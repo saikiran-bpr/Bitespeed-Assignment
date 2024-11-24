c4863ed3b622a94db1dd9d5ecca69b2438767e3d
Bompelliwar Sai Kiran <bompelliwarsaikiran@Sai-Kirans-MacBook-Pro.local>
2024-11-24 15:25:18 +0530

    removed validators

--- src/controllers/identify.ts
+++ src/controllers/identify.ts
@@ -1,11 +1,5 @@
 import { Request, Response } from "express";
 import ContactService from "../services/identify";
-import { z } from "zod";
-
-const contactSchema = z.object({
-    email: z.string().email({ message: "Please provide a valid email" }).optional(),
-    phoneNumber: z.string().regex(/^\d{10}$/, { message: "Phone number must be 10 digits" }).optional()
-});
 
 export const postData = async (req: Request, res: Response): Promise<any> => {
     const { email, phoneNumber } = req.body;
@@ -13,14 +7,6 @@ export const postData = async (req: Request, res: Response): Promise<any> => {
     if (!email && !phoneNumber) {
         return res.status(400).json({
             message: "Please provide email or phone number",
-        });
-    }
-
-    const valid = contactSchema.safeParse(req.body);
-    if (!valid.success) {
-        return res.status(400).json({
-            message: "Validation Error",
-            errors: valid.error.errors,
         });
     }
 
